93f381fb07e2bf35ad86b7a8e60de029ea41c3c8
Md Ejaj Mahmud (MY) <ejaj.mahmud@shinjiru.com.my>
2026-05-31 01:42:24 +0800

    Compile and embed live Firestore database connection options

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "964672359" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2708609603" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
